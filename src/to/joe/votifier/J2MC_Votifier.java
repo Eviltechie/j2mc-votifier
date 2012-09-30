adc7c9a5e9f0e8351dd6a20872967d5213cfdf15
Ivan Panarusky <eviltechie@joe.to>
2012-09-29 20:32:16 -0400

    basic sql logging

--- src/to/joe/votifier/J2MC_Votifier.java
+++ src/to/joe/votifier/J2MC_Votifier.java
@@ -1,10 +1,15 @@
 package to.joe.votifier;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import to.joe.j2mc.core.J2MC_Manager;
 
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
@@ -27,6 +32,17 @@ public class J2MC_Votifier extends JavaPlugin implements Listener {
     public void onVote(VotifierEvent event) {
         Vote v = event.getVote();
         Logger l = getLogger();
+        
+        try {
+            PreparedStatement ps = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("INSERT INTO votes (address, service, timestamp, username) VALUES (?,?,?,?)");
+            ps.setString(1, v.getAddress());
+            ps.setString(2, v.getServiceName());
+            ps.setString(3, v.getTimeStamp());
+            ps.setString(4, v.getUsername());
+            ps.execute();
+        } catch (SQLException e) {
+            getLogger().log(Level.SEVERE, "Something went wrong logging the vote in the database", e);
+        }
         
         l.info(v.getAddress());
         l.info(v.getServiceName());
